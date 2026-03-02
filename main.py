bb65011a425a71e9a347bed4ed7140e7e097be1b
unknown <andrei05.jules22@gmail.com>
2026-03-03 02:18:33 +0800

    update

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 from settings import *
+from player import Player
 
 class Game:
     def __init__(self):
+        # setup
         pygame.init()
         self.display_surface = pygame.display.set_mode((window_width, window_height))
         pygame.display.set_caption('Isekai Survivor')
         self.clock = pygame.time.Clock()
         self.running = True
+
+        # groups
+        self.all_sprites = pygame.sprite.Group()
+
+        #sprites
+        self.player = Player((400,300), self.all_sprites)
+
     def run(self):
         while self.running:
             # dt
@@ -16,11 +25,15 @@ class Game:
                 if event.type == pygame.QUIT:
                     self.running = False
             # update
+            self.all_sprites.update(dt)
 
             # draw
+            self.display_surface.fill('black')
+            self.all_sprites.draw(self.display_surface)
             pygame.display.update()
         
         pygame.quit()
 
-game = Game()
-game.run()
+if __name__ == '__main__':
+    game = Game()
+    game.run()
--- settings.py
+++ settings.py
@@ -2,5 +2,5 @@ import pygame
 from os.path import join
 from os import walk
 
-window_width, window_height = 1280, 720
+window_width, window_height = 1920, 1080
 tile_size = 64
