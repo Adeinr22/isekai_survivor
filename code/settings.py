e2c372c89b773165bc74c8e2bb5a0ca0e16684cc
Adeinr22 <andrei05.jules22@gmail.com>
2026-03-04 12:25:19 +0800

    added Ui colors

--- code/settings.py
+++ code/settings.py
@@ -4,3 +4,11 @@ from os import walk
 
 window_width, window_height = 1920, 1080
 tile_size = 64
+
+# UI Colors
+health_color = (255, 0, 0)
+health_bg_color = (100, 0, 0)
+xp_color = (0, 255, 0)
+xp_bg_color = (0, 100, 0)
+ui_font_color = (255, 255, 255)
+ui_font_size = 36
